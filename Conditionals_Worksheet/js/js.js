3aa08ad5a0e08383874b7f02e38a2f15091ce247
Christian Heyd <christian.heyd@gmail.com>
2013-05-19 14:26:18 -0400

    Conditionals Worksheet update
    Finished Stuff Your Face I

---
+++ Conditionals_Worksheet/js/js.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+// Christian Heyd
+// Conditionals Worksheet
+// May 19th, 2013
+
+// Stuff Your Face I
+
+console.log("Enter competitor weight!");
+
+var weight = prompt("Enter competitor weight.");
+console.log("Competitor weight: " + weight);
+
+
+//If greater than 250, qualify for heavyweight division.
+
+if(weight > 250){
+	console.log("The competitor qualifies for the heavyweight division.");
+	alert("The competitor qualifies for the heavyweight division.");
+	
+	}
+	else{
+	console.log("The competitor needs to gain some weight!");
+	alert("The competitor needs to gain some weight!");
+		
+	};
+	
+
+	
+
+	
+	
+//Temperature Converter
