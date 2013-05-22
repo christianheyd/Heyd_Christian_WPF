acefeb8a403d302d3c63e6abe7dbfb7db00058b5
Christian Heyd <christian.heyd@gmail.com>
2013-05-22 19:49:38 -0400

    Started adding vars for entering current time.

--- Assignment_Conditionals/Personal/js/script.js
+++ Assignment_Conditionals/Personal/js/script.js
@@ -5,4 +5,7 @@
  
 /*This script will tell you if you are getting a healthy amount of sleep.
   I chose this because I'm currently trying to fix my sleep schedule!*/
-   
+
+alert("Is it time for you to go to sleep?");
+var currentHour = prompt("Enter the current hour.");
+var currentMinute = prompt("Enter current minute");
