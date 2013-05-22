487507f7017685409939976913725daece58f2a7
Christian Heyd <christian.heyd@gmail.com>
2013-05-22 19:46:50 -0400

    Outlined goal for Personal script.js

--- Assignment_Conditionals/Personal/js/script.js
+++ Assignment_Conditionals/Personal/js/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+/*Christian Heyd
+ *May 22nd, 2013
+ *Assignment: Conditionals - Personal*/
+ 
+/*This script will tell you if you are getting a healthy amount of sleep.
+  I chose this because I'm currently trying to fix my sleep schedule!*/
+   
