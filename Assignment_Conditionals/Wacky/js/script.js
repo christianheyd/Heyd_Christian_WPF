cbe9dbd7e6ee5bd74059181d86027a06394c15cd
Christian Heyd <christian.heyd@gmail.com>
2013-05-23 00:40:33 -0400

    Initial script setup.

--- Assignment_Conditionals/Wacky/js/script.js
+++ Assignment_Conditionals/Wacky/js/script.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+/*
+Christian Heyd
+May 23, 2013
+Assignment: Conditionals - Wacky
+
+This script is based on something I was trying to do in the prior assignment, but
+abandoned the idea because I couldn't do what I wanted to yet. I decided to start over and 
+make it work.
+
+Space Weights
+*/
+
+alert("How much would you weight on another planet?");
+var weight = prompt("How much do you weigh on Earth?");
+console.log(weight);
