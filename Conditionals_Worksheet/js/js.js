eb3524394f7adb53415f834b785c7299cb961633
Christian Heyd <christian.heyd@gmail.com>
2013-05-23 21:47:00 -0400

    Conditionals Worksheet: Added Grade Letter Calculator
    commented out other scripts for testing.

--- Conditionals_Worksheet/js/js.js
+++ Conditionals_Worksheet/js/js.js
@@ -5,14 +5,14 @@
 
 // Stuff Your Face I
 
-console.log("Enter competitor weight!");
+/*console.log("Enter competitor weight!");
 
 var weight = prompt("Enter competitor weight.");
-console.log("Competitor weight: " + weight);
+console.log("Competitor weight: " + weight);*/
 
 
 //If greater than 250, qualify for heavyweight division.
-
+/*
 if(weight > 250){
 	console.log("The competitor qualifies for the heavyweight division.");
 	alert("The competitor qualifies for the heavyweight division.");
@@ -24,14 +24,14 @@ if(weight > 250){
 		
 	};
 	
-
+*/
 	
 
 	
 	
 //Temperature Converter
 
-var temp = prompt("This will convert a temperature from Fahrenheit to Celsius, or vice versa.\nPlease enter the temperature.");
+/*var temp = prompt("This will convert a temperature from Fahrenheit to Celsius, or vice versa.\nPlease enter the temperature.");
 console.log(temp);
 
 var tempSystem = prompt("Please enter C or F.");
@@ -45,5 +45,15 @@ if(tempSystem == "F"){
 	var conversionF = (temp  -  32)  *  5/9;
 	
 	console.log("The temperature in Celsius is " + conversionF + (" degrees."));
-};
+};*/
 
+// Grade Letter Calculator
+
+var grade = prompt("Please enter your grade.");
+if(parseInt(grade) < 69){
+	console.log("You got " + grade + " percent, so you got an F! :(");
+}else if(grade < 72){
+	console.log("You got " + grade + " percent, so you got a D!");
+}else if(grade < 75){
+	console.log("You got " + grade + " percent, so you got a C!");
+}else{};
