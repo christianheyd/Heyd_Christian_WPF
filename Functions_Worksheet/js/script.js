deba3004d0a89beebd9ff6095f30d371f6fb435f
Christian Heyd <christian.heyd@gmail.com>
2013-05-28 16:10:50 -0400

    Basic function setup and prompt added for radius

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+// Christian Heyd
+// May 28th, 2013
+// Functions Worksheet
+
+//Circumference
+
+var radius = prompt("Please enter radius of the circle.");
+
+function(radius){
+	var diameter = radius * 2;
+	var circ = 3.14 * diameter;
+	return circ;
+	};
