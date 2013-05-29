d7db6cc2d93374b8fc5c615952a0cb5bea567894
Christian Heyd <christian.heyd@gmail.com>
2013-05-29 16:41:37 -0400

    Changed spacing on Circumference for organization. Almost finished with Stung.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -11,12 +11,18 @@ function circumference(r){//function to calculate circumference
 	var diameter = r * 2; //finds diameter of circle
 	var circ = 3.14 * diameter; //calculates cicumference
 	return circ; //returns circumference
-	};
+};
 	
-	var circLog = circumference(radius); //return variable for circumference
-	console.log(circLog); //prints circumference to console.
+var circLog = circumference(radius); //return variable for circumference
+console.log(circLog); //prints circumference to console.
 
 // Stung!
 
 var weight = prompt("Please enter your weight to see how many beestings are fatal.");
 
+function sting(w){
+	var stingsFatal = 8.666666667 * w;
+	return stingsFatal;
+};
+
+var ouch = sting(weight);
