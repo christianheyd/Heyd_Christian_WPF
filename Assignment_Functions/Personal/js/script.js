7683297b9a4fdb1d0b80c898a7e30d39580b7388
Christian Heyd <christian.heyd@gmail.com>
2013-05-29 18:33:35 -0400

    Began writing function for personal.

--- Assignment_Functions/Personal/js/script.js
+++ Assignment_Functions/Personal/js/script.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+// Christian Heyd
+// May 29th, 2013
+// Assignment: Functions - Personal
+
+/* I thought of creating this tool for my own personal use beyond this class because of a rule I was taught when I was
+   younger related to saving money (something I've always had a tough time with). The rule is to never spend more than
+   a quarter of your total amount available to save while earning. I will demonstrate this with code here. */
+   
+alert("This tool will help calculate if a purchase is within your budget.");
+
+var totalBalance = prompt("Please enter your current balance.");
+console.log("Balance: $" + totalBalance);
+
+var purchaseAmount = prompt("Please enter the amount of your desired purchase.");
+console.log("Purchase amount: $" + purchaseAmount);
+
+function quarter(b){
+	var quarterBalance = b / 4;
+	return quarterBalance
+
+};
