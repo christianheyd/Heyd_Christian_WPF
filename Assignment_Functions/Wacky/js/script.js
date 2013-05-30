7111d506b8a9533a2e7b48b814679a03a2c32310
Christian Heyd <christian.heyd@gmail.com>
2013-05-30 14:41:32 -0400

    Added Wacky index.html and script.js and linked them.

---
+++ Assignment_Functions/Wacky/js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+// Christian Heyd
+// May 30th, 2013
+// Functions - Wacky
+
+/* I'm currently writing this code at an airport and thought it'd be cool to calculate how full the plane is.*/
+
+alert("How full is your plane?");
+var planeSize = prompt("How many seats are on the plane?");
