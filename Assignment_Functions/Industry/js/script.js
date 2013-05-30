a3b407d2844e971f0dd7cf5355450a5f2603beea
Christian Heyd <christian.heyd@gmail.com>
2013-05-29 23:13:46 -0400

    Basic setup of script.js with outline added

--- Assignment_Functions/Industry/js/script.js
+++ Assignment_Functions/Industry/js/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+// Christian Heyd
+// May 29th, 2013
+// Functions - Industry
+
+/* I came up with this idea to help me break up larger projects into smaller ones for time management, which
+   I personally think is one of the most important skills to learn in the web design industry.*/
+   
+alert("How much have you done on your project for the client?");
+
+var tasksTotal = prompt("How many tasks are there in total to complete?");
+
+var tasksCompleted = prompt("How many tasks have been completed?");
+
