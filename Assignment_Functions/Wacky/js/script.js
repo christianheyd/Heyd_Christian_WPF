2ff75a33d6643dd416c636ed623d30645a61181c
Christian Heyd <christian.heyd@gmail.com>
2013-05-30 14:48:56 -0400

    Added function for percentage of seats taken.

--- Assignment_Functions/Wacky/js/script.js
+++ Assignment_Functions/Wacky/js/script.js
@@ -7,3 +7,12 @@
 
 alert("How full is your plane?");
 var planeSize = prompt("How many seats are on the plane?");
+console.log(planeSize);
+
+var passengers = prompt("How many passengers are on the plane?");
+
+function planePercent(){
+	var percent = p / s;
+	var convert = percent * 100;
+	return convert;
+}
