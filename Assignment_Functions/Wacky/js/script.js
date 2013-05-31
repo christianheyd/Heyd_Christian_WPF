f58b5023d500209d2f4d3ac460304dd8a1abd0b3
Christian Heyd <christian.heyd@gmail.com>
2013-05-30 21:46:56 -0400

    Finished wacky script on the plane. Couldn't commit in the air
    Final

--- Assignment_Functions/Wacky/js/script.js
+++ Assignment_Functions/Wacky/js/script.js
@@ -6,17 +6,27 @@
 /* I'm currently writing this code at an airport and thought it'd be cool to calculate how full the plane is.*/
 
 alert("How full is your plane?");
-var planeSize = prompt("How many seats are on the plane?");
-console.log(planeSize);
 
-var passengers = prompt("How many passengers are on the plane?");
+var planeSize = prompt("How many seats are on the plane?"); /*Prompts user for number of seats on plane.*/
+
+var passengers = prompt("How many passengers are on the plane?"); /*Prompts user for number of passengers on plane.*/
 
 function planePercent(p, s){
-	var percent = p / s;
+	/*The first var converts the values from the previous prompts into a decimal, while the second one turns the decimal 
+	into a whole number for display in the console.*/
+	var percent = p / s; 
 	var convert = percent * 100;
 	return convert;
 }
 
-var askPercentDisplay = prompt("Would you like to see percent full or empty? \nEnter 1 for full and 2 for empty.");
+var askPercentDisplay = prompt("Would you like to see percent full or empty? \nEnter 1 for full and 2 for empty."); /*Prompts user for which percent they
+wish to view. It all depends on whether they're an optimist or a pessimist!*/
 
-if(askPercentDisplay == "1")
+if(askPercentDisplay == "1"){/*if else statement for displaying proper percent.*/
+	var fullPercent = planePercent(passengers, planeSize);
+	console.log("The plane is " + fullPercent + " percent full!");
+}else{
+	var emptyPercent = planePercent(passengers, planeSize);
+	var empty = 100 - emptyPercent;
+	console.log("The plane is " + empty + " percent empty!");
+}
